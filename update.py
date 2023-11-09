44cb532cc285a8258b68d76a61ef23b03a82a1f5
Ravi_Teja_Racha <raviteja.racha8@gmail.com>
2023-11-09 13:11:03 +0530

    first commit

---
+++ update.py
@@ -0,0 +1,24 @@
+# installed library mysql-connector-python
+import mysql.connector
+
+#creating connection 
+
+class update:
+    def __init__(self):
+        self.conn=mysql.connector.connect(
+                host = "localhost",
+                user = "root",
+                password = "ROOT",
+                database = "bank"
+                )
+        
+    def myupdate(self,table_name,column_name,new_value,cusid):
+        cur = self.conn.cursor()
+        if new_value.isnumeric():
+            print("test1")
+            cur.execute(f"UPDATE {table_name} set {column_name}={int(new_value)} where customerID={cusid}")
+        else:
+            print("test2")
+            cur.execute(f"UPDATE {table_name} set {column_name}='{new_value}' where customerID={cusid}")
+        self.conn.commit()
+        print("updated sucessfully")
